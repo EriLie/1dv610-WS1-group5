29c24529d44fc30f8cf53454417a9ef0e9420c64
EriLie <le222vp@student.lnu.se>
2019-09-10 10:18:34 +0200

    Creating class for TranslateView

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-wiho
+<?php
+
+namespace view;
+
+class TranslateView {
+    // array containing strings
+    private $title = 'Translator group 5 - Rövarspråket'; 
+    private $charset = 'utf-8';
+
+    // Textarea input
+    private $input;
+
+    // Textarea output
+    private $output;
+    
+
+    // Function that translates rövarspråk
+
+    // rendering page
+    public function echoHTML() {
+        echo " 
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset='$this->charset'>
+                <title>$this->title</title>
+            </head>
+            <body>
+                $this->input
+                <br>
+                $this->output
+            </body>
+        </html>";
+
+    }
+
+
+
+}
+
